b5934a2463c92286b68a2325760308d6727e7a35
eugabiviana <gabi.viana88@gmail.com>
2023-10-27 19:02:02 -0300

    Features: Optimizing imports

--- attornatus/src/main/java/com/desafio/attornatus/dtos/endereco/EnderecoDTO.java
+++ attornatus/src/main/java/com/desafio/attornatus/dtos/endereco/EnderecoDTO.java
@@ -1,12 +1,7 @@
 package com.desafio.attornatus.dtos.endereco;
 
-import com.desafio.attornatus.dtos.pessoa.PessoaDTO;
-import com.desafio.attornatus.models.endereco.Endereco;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class EnderecoDTO {
 
