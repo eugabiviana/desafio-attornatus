c96e70ddb090ee59bbbb8dfc82a8dedccd9107c9
eugabiviana <gabi.viana88@gmail.com>
2023-10-27 17:16:21 -0300

    Feature: Complementing Model

--- attornatus/src/main/java/com/desafio/attornatus/models/Pessoa.java
+++ attornatus/src/main/java/com/desafio/attornatus/models/Pessoa.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -19,4 +20,56 @@ public class Pessoa {
     @NotNull
     private Date dataNasc;
 
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public Pessoa() {
+
+    }
+    public Pessoa(UUID id) {
+        this.setId(UUID.randomUUID());
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", dataNasc=" + dataNasc +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pessoa pessoa)) return false;
+        return Objects.equals(id, pessoa.id) && Objects.equals(nome, pessoa.nome) && Objects.equals(dataNasc, pessoa.dataNasc);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, dataNasc);
+    }
 }
