f36905c3284e34e657e59db930515e55a815f002
eugabiviana <gabi.viana88@gmail.com>
2023-10-27 17:20:57 -0300

    Feature: Adding Annotation

---
+++ attornatus/src/main/java/com/desafio/attornatus/controlers/PessoaController.java
@@ -0,0 +1,4 @@
+package com.desafio.attornatus.controlers;
+
+public class PessoaController {
+}
--- attornatus/src/main/java/com/desafio/attornatus/repositories/PessoaRepository.java
+++ attornatus/src/main/java/com/desafio/attornatus/repositories/PessoaRepository.java
@@ -1,6 +1,8 @@
 package com.desafio.attornatus.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PessoaRepository extends JpaRepository {
 }
