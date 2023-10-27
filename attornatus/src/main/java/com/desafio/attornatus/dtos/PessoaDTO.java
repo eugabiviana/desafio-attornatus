f3dbe7f73cdcd014ca85717b894f01276547e4fc
eugabiviana <gabi.viana88@gmail.com>
2023-10-27 17:18:26 -0300

    Feature: Creating PessoaDTO

---
+++ attornatus/src/main/java/com/desafio/attornatus/dtos/PessoaDTO.java
@@ -0,0 +1,29 @@
+package com.desafio.attornatus.dtos;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+
+public class PessoaDTO {
+
+    @NotNull
+    private String nome;
+    @NotNull
+    private Date dataNasc;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+}
