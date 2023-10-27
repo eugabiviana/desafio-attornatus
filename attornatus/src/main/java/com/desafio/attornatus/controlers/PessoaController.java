55062b1a1765e7f743647c88cfd7e057da9c7738
eugabiviana <gabi.viana88@gmail.com>
2023-10-27 17:32:03 -0300

    Feature: Creating Post Method

--- attornatus/src/main/java/com/desafio/attornatus/controlers/PessoaController.java
+++ attornatus/src/main/java/com/desafio/attornatus/controlers/PessoaController.java
@@ -1,4 +1,32 @@
 package com.desafio.attornatus.controlers;
 
+import com.desafio.attornatus.dtos.PessoaDTO;
+import com.desafio.attornatus.models.Pessoa;
+import com.desafio.attornatus.repositories.PessoaRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/cadastros")
 public class PessoaController {
+
+    @Autowired
+    PessoaRepository repository;
+
+    @PostMapping
+    public ResponseEntity<Pessoa> save(@RequestBody @Valid PessoaDTO pessoaDTO){
+        var pessoa = new Pessoa();
+        BeanUtils.copyProperties(pessoaDTO, pessoa);
+        repository.save(pessoa);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(pessoa);
+    }
 }
