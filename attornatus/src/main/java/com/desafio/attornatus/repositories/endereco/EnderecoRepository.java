604d0a7600fa6e76b3a11689e273537730691f20
eugabiviana <gabi.viana88@gmail.com>
2023-10-27 18:21:52 -0300

    Features: Creating EnderecoRepository

---
+++ attornatus/src/main/java/com/desafio/attornatus/repositories/endereco/EnderecoRepository.java
@@ -0,0 +1,8 @@
+package com.desafio.attornatus.repositories.endereco;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository {
+}
